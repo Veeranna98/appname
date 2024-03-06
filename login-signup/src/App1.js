965ac768a9f7ed3a3014b965081f563da6573100
veeranna <veeranna@lyrostech.com>
2024-03-06 18:30:39 +0530

    spraed methods

---
+++ login-signup/src/App1.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function App1() {
+    const handleClick=() =>
+{
+    alert('hello world')
+}
+  return (
+    <div>
+        <button onClick={handleClick}>here</button>
+    </div>
+  )
+}
